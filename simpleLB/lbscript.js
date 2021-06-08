0b4965f667ce90b5f745b2368f3362a543bdf1a9
vojone <75688455+vojone@users.noreply.github.com>
2021-06-08 22:57:08 +0200

    Other refactoring (function creating buttons was made)

--- simpleLB/lbscript.js
+++ simpleLB/lbscript.js
@@ -1,101 +1,103 @@
 ﻿// JavaScript Document
 
-lb_name = ""; //label for downloaded photos ("lb_name"_"number".jpg)
+lbName = ""; //label for downloaded photos ("lbName"_"number".jpg)
+srcImagesDir = "simpleLB/images/";
 
-$(document).ready(function(){
+$(document).ready(function() {
 
     //Prepare lb window in background
-    lb_page = document.createElement("div");
-    lb_page.id = "lb_cont";
-    document.getElementsByTagName("html")[0].appendChild(lb_page);
-    lb_photo = document.createElement("img");
-    lb_photo.id = "lb_img";
-    lb_page.appendChild(lb_photo);
-    lb_page.addEventListener("click", lb_close);
+    page = document.createElement("div");
+    page.id = "lb_cont";
+    document.getElementsByTagName("html")[0].appendChild(page);
+    photo = document.createElement("img");
+    photo.id = "lb_img";
+    page.appendChild(photo);
+    page.addEventListener("click", close);
 
-    lb_page.style.display = "none"; //lb is not showed when page is loaded
+    page.style.display = "none"; //lb is not showed when page is loaded
       
     //Get images
-    lb_images = document.getElementsByClassName("lb");
+    images = document.getElementsByClassName("lb");
     
     //Add onclick attributte
-    for(i = 0; lb_images[i] != null; i++) {
-        lb_images[i].setAttribute("onclick", "lb(" + i + ")");
+    for(i = 0; images[i] != null; i++) {
+        images[i].setAttribute("onclick", "lb(" + i + ")");
     } 
 
     $(document).keydown(function(keyPressed) {
         keyHandler(keyPressed);
     });
-
-    //Close button 
-    lb_cross = document.createElement("img");
-    lb_cross.src = "simpleLB/images/lb_close.png";
-    lb_cross.id = "lb_cross";
-    lb_cross.title = "Close galery";
-    lb_cross.addEventListener("click", lb_backToWeb);
-    lb_page.appendChild(lb_cross);
     
     //Downloading button
-    lb_down_link = document.createElement("a");
-    lb_down_link.title = "Download image";
-    lb_page.appendChild(lb_down_link);
-    lb_down = document.createElement("img");
-    lb_down.src = "simpleLB/images/download.png";
-    lb_down.id = "lb_download";
-    lb_down_link.appendChild(lb_down);
+    downLink = document.createElement("a");
+    downLink.title = "Download image";
+    page.appendChild(downLink);
+
+    down = document.createElement("img");
+    down.src = "simpleLB/images/download.png";
+    down.id = "lb_download";
+    downLink.appendChild(down);
+
+    //Close button 
+    crossButton = createEl("img", "lb_close.png", "Close galery", "lb_cross", backToWeb);
+    page.appendChild(crossButton);
     
     //Next photo button
-    lb_next = document.createElement("img");
-    lb_next.src = "simpleLB/images/next.png";
-    lb_next.title = "Next image";
-    lb_next.id = "lb_next";
-    lb_next.addEventListener("click", lb_nextPh);
-    lb_page.appendChild(lb_next);
+    nextButton = createEl("img", "next.png", "Next image", "lb_next", nextPh);
+    page.appendChild(nextButton);
     
     //Previous photo button
-    lb_back = document.createElement("img");
-    lb_back.src = "simpleLB/images/back.png";
-    lb_back.title = "Previous image";
-    lb_back.id = "lb_back";
-    lb_back.addEventListener("click", lb_prevPh); 
-    lb_page.appendChild(lb_back);
+    backButton = createEl("img", "back.png", "Previous image", "lb_back", prevPh);
+    page.appendChild(backButton);
 });
+
+//Returns created html element
+function createEl(type, bg, title, id, event) {
+    newEl = document.createElement(type);
+    newEl.src = srcImagesDir + bg;
+    newEl.id = id;
+    newEl.title = title;
+    newEl.addEventListener("click", event);
+    
+    return newEl;
+}
 
 //Solves pressed key
 function keyHandler(key) {
-    lb_state = document.getElementById("lb_cont").style.display;
 
-    if(lb_state != "none") {
+    pageState = page.style.display;
+
+    if(pageState != "none") {
         switch(key.code || key.which) {
             case "ArrowLeft":
-                lb_prevPh();
+                prevPh();
                 break;
             case "ArrowRight":
-                lb_nextPh();
+                nextPh();
                 break;
             case "Escape":
-                lb_backToWeb();
+                backToWeb();
                 break;
         }
     }
 }
 
 //Shows window with lb and initializes it with photo given in argument
-function lb(lb_im = 0) {
+function lb(im = 0) {
     $("#lb_cont").fadeIn();
-    lb_photo.src = lb_images[lb_im].src;
-    lb_image = lb_im;
-    lb_down_link.href =  lb_images[lb_im].src;
-    lb_down_link.download = lb_name + "_" + lb_im;    
+    photo.src = images[im].src;
+    image = im;
+    downLink.href =  images[im].src;
+    downLink.download = lbName + "_" + im;    
 }
 
 //Hides window with lb
-function lb_backToWeb() {
+function backToWeb() {
     $("#lb_cont").fadeOut();       
 }
 
 //Hides window with lb when user clicks somewhere out of photo and buttons
-function lb_close(click) {
+function close(click) {
     var x = document.getElementById("lb_img"); 
     var el = click.target;
     
@@ -117,29 +119,29 @@ function lb_close(click) {
         }
     }
 
-    lb_backToWeb();
+    backToWeb();
 }
 
 //loads next photo (when it is last photo go to start)
-function lb_nextPh() {
-    lb_image++;
-    if (lb_image > (lb_images.length - 1)) {
-      lb_image = 0;
+function nextPh() {
+    image++;
+    if (image > (images.length - 1)) {
+      image = 0;
     }
     
-    lb_photo.src = lb_images[lb_image].src;
-    lb_down_link.href = lb_images[lb_image].src;
-    lb_down_link.download = lb_name + "_" + lb_image;    
+    photo.src = images[image].src;
+    downLink.href = images[image].src;
+    downLink.download = lbName + "_" + image;    
 }
 
 //loads previous photo
-function lb_prevPh() {
-    lb_image--;
-    if (lb_image < 0) {
-      lb_image = lb_images.length - 1;
+function prevPh() {
+    image--;
+    if (image < 0) {
+      image = images.length - 1;
     }
 
-    lb_photo.src = lb_images[lb_image].src;
-    lb_down_link.href =  lb_images[lb_image].src;
-    lb_down_link.download = lb_name + "_" + lb_image;    
+    photo.src = images[image].src;
+    downLink.href =  images[image].src;
+    downLink.download = lbName + "_" + image;    
 }
