802028d525ad541050dd2046746f7967d9345b22
vojone <75688455+vojone@users.noreply.github.com>
2021-06-09 09:30:22 +0200

    Added background blur while lb is active

--- simpleLB/lbscript.js
+++ simpleLB/lbscript.js
@@ -1,13 +1,16 @@
 ﻿// JavaScript Document
 
 lbName = ""; //label for downloaded photos ("lbName"_"number".jpg)
-srcImagesDir = "simpleLB/images/";
+srcImagesDir = "simpleLB/images/"; //path to source images (e.g. for buttons)
+blurFlag = true; //if true body is blurred while is lightbox used (!set bgAlpha to <> 1!)
+bgAlpha = 0.9; //bg transparency (1 is completely black, 0 is completely transparent)
 
 $(document).ready(function() {
 
     //Prepare lb window in background
     page = document.createElement("div");
     page.id = "lb_cont";
+    page.style.backgroundColor = "rgba(0, 0, 0, " + bgAlpha + ")"; 
     document.getElementsByTagName("html")[0].appendChild(page);
     photo = document.createElement("img");
     photo.id = "lb_img";
@@ -23,10 +26,6 @@ $(document).ready(function() {
     for(i = 0; images[i] != null; i++) {
         images[i].setAttribute("onclick", "lb(" + i + ")");
     } 
-
-    $(document).keydown(function(keyPressed) {
-        keyHandler(keyPressed);
-    });
     
     //Downloading button
     downLink = document.createElement("a");
@@ -49,6 +48,11 @@ $(document).ready(function() {
     //Previous photo button
     backButton = createEl("img", "back.png", "Previous image", "lb_back", prevPh);
     page.appendChild(backButton);
+
+    
+    $(document).keydown(function(keyPressed) {
+        keyHandler(keyPressed);
+    });
 });
 
 //Returns created html element
@@ -64,7 +68,6 @@ function createEl(type, bg, title, id, event) {
 
 //Solves pressed key
 function keyHandler(key) {
-
     pageState = page.style.display;
 
     if(pageState != "none") {
@@ -85,6 +88,11 @@ function keyHandler(key) {
 //Shows window with lb and initializes it with photo given in argument
 function lb(im = 0) {
     $("#lb_cont").fadeIn();
+
+    if(blurFlag) {
+        $("body").css("filter", "blur(3px)");
+    }
+
     photo.src = images[im].src;
     image = im;
     downLink.href =  images[im].src;
@@ -93,7 +101,12 @@ function lb(im = 0) {
 
 //Hides window with lb
 function backToWeb() {
-    $("#lb_cont").fadeOut();       
+    $("#lb_cont").fadeOut(); 
+
+    if(blurFlag) {
+        $("body").css("filter", "none");
+    }
+
 }
 
 //Hides window with lb when user clicks somewhere out of photo and buttons
